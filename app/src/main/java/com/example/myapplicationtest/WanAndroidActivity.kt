b9dba9337478c558c023c91c371c461c93dccfb3
govech <1299940308@qq.com>
2023-10-21 22:59:14 +0800

    优化代码

--- app/src/main/java/com/example/myapplicationtest/WanAndroidActivity.kt
+++ app/src/main/java/com/example/myapplicationtest/WanAndroidActivity.kt
@@ -2,13 +2,10 @@ package com.example.myapplicationtest
 
 import android.os.Bundle
 import com.example.myapplicationtest.base.BaseActivity
-import com.example.myapplicationtest.databinding.ActivityWanAndroidBinding
-import com.example.myapplicationtest.ktx.binding
 import com.example.myapplicationtest.ktx.createRecycleRView
 import com.example.myapplicationtest.ktx.startActivityKt
 
 class WanAndroidActivity : BaseActivity() {
-    private val mBinding by binding(ActivityWanAndroidBinding::inflate)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initRv()
@@ -21,12 +18,13 @@ class WanAndroidActivity : BaseActivity() {
                 "page3" -> {
 
                 }
+
                 else -> {
                     startActivityKt<HomeArticleActivity>()
                 }
             }
 
         }
-        mBinding.root.addView(recyclerView)
+        setContentView(recyclerView)
     }
 }
