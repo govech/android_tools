319be430c39726ff4d7ea9546d542bde9fe70edd
cairong <1299940308@qq.com>
2023-10-27 22:05:57 +0800

    fix:处理无法完整加载公众号文章问题

--- app/src/main/java/com/example/myapplicationtest/activitys/WebActivity.kt
+++ app/src/main/java/com/example/myapplicationtest/activitys/WebActivity.kt
@@ -21,6 +21,7 @@ class WebActivity : BaseActivity() {
         mBinding.webArticle.loadUrl(mUrl.toString())
         mBinding.webArticle.settings.apply {
 
+            domStorageEnabled = true//处理无法完整加载公众号文章问题
 
             javaScriptEnabled = true //如果访问的页面中要与Javascript交互，则webview必须设置支持Javascript，若加载的 html 里有JS 在执行动画等操作，
                                      // 会造成资源浪费（CPU、电量），在 onStop 和 onResume 里分别把 setJavaScriptEnabled() 给设置成 false 和 true 即可
