658bb7ea9016475b92e956d2f60afc30b780bf38
cairong <1299940308@qq.com>
2023-11-15 19:41:38 +0800

    增加扩展函数：用于增大View的点击面积

--- app/src/main/java/com/example/myapplicationtest/extensions/views.kt
+++ app/src/main/java/com/example/myapplicationtest/extensions/views.kt
@@ -1,8 +1,12 @@
+import android.graphics.Rect
 import android.os.Build
+import android.view.MotionEvent
+import android.view.TouchDelegate
 import android.view.View
 import android.view.ViewTreeObserver
 import androidx.lifecycle.lifecycleScope
 import com.example.myapplicationtest.base.IUiView
+import kotlinx.coroutines.NonDisposableHandle.parent
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.onCompletion
@@ -65,3 +69,41 @@ fun View.click(a: () -> Unit) {
         a.invoke()
     }
 }
+
+
+// 增大view点击面积
+fun View.setCustomTouchDelegate(
+    left: Int = 0,
+    top: Int = 0,
+    right: Int = 0,
+    bottom: Int = 0,
+    clickAction: () -> Unit
+) {
+    // 获取视图的矩形范围
+    val viewRect = Rect()
+    getHitRect(viewRect)
+
+    // 调整矩形参数
+    val expandedRect =
+        Rect(
+            viewRect.left - left,
+            viewRect.top - top,
+            viewRect.right + right,
+            viewRect.bottom + bottom
+        )
+
+    // 创建一个 TouchDelegate 实例
+    val touchDelegate = object : TouchDelegate(expandedRect, this) {
+        override fun onTouchEvent(event: MotionEvent): Boolean {
+            // 处理点击事件的逻辑
+            clickAction.invoke()
+            return super.onTouchEvent(event)
+        }
+    }
+
+    // 将 TouchDelegate 设置给 View 的父级
+    (parent as? View)?.let {
+        it.touchDelegate = touchDelegate
+    }
+}
+
